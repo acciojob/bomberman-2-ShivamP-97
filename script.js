fe5281bcac0ab4c9c7b63e080255d4ba3b84a75a
ShivamP-97 <shivampathak19972@gmail.com>
2025-12-02 22:35:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-//your code here
 
 function reset() {
-    for (i = 1; i <= 9; i++) {
+    for (let i = 1; i <= 9; i++) {
         const block = document.getElementById(`${i}`);
         block.style.backgroundColor = "transparent";
     }
 }
-  
+
 document.getElementById('reset_button').addEventListener('click', reset);
 
 document.getElementById('change_button').addEventListener('click', () => {
     reset();
+
     const blockId = document.getElementById("block_id").value;
     const color = document.getElementById("colour_id").value;
-    // alert(colorId)
-    const block = document.getElementById(`${blockId}`);
+
+    const block = document.getElementById(blockId);
+
+    if (!block) {
+        alert("Invalid block ID! Enter a number 1–9.");
+        return;
+    }
+
     block.style.backgroundColor = color;
 });
